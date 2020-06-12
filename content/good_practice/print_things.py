605543e5c71607c89e31d04e788b54b2714991ce
William Glass <wglass149@googlemail.com>
2020-06-12 16:18:27 +0100

    start of packages intro

---
+++ content/good_practice/print_things.py
@@ -0,0 +1,7 @@
+def print_names(list_of_names):
+    for name in list_of_names:
+        print(name)
+
+def print_dictionary(input_dictionary):
+    for key, value in input_dictionary.items():
+        print("Key: " + key + ", Value: " + str(value))
