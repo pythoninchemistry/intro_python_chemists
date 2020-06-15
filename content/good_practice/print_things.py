cc21c0c7aa538a7899e77566b7298f450eb1c5c2
William Glass <wglass149@googlemail.com>
2020-06-15 15:20:51 +0100

    Update custom_modules notebook

--- content/good_practice/print_things.py
+++ content/good_practice/print_things.py
@@ -5,3 +5,6 @@ def print_names(list_of_names):
 def print_dictionary(input_dictionary):
     for key, value in input_dictionary.items():
         print("Key: " + key + ", Value: " + str(value))
+
+def _private_function():
+    print("I'm a private function!")
